244869c496df93777ef5443521bb24a2c0e93405
Weshesh <andi.angel@boheemia.ee>
2020-10-13 20:17:53 +0300

    Add ProjectAvailabilityBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ProjectAvailabilityBar } from './ProjectAvailabilityBar';
 
 function App() {
   return (
     <div className="App bg-gray-100 flex content-center p-20 w-screen h-screen">
       <div className="container mx-auto bg-white border-2 border-gray-200 rounded-3xl p-5 h-auto shadow-2xl">
         Kana
+        <ProjectAvailabilityBar></ProjectAvailabilityBar>
       </div>
     </div>
   );
---
+++ src/ProjectAvailabilityBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function ProjectAvailabilityBar() {
+  return (
+    <div className="w-full border-b-2">
+
+    </div>
+  )
+}
