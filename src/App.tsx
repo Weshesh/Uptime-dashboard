49dc7c947e75126ede4134a846765aa195f8fade
Weshesh <andi.angel@boheemia.ee>
2020-10-13 20:10:55 +0300

    Add dashboard container

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gray-100 flex content-center p-20 w-screen h-screen">
+      <div className="container mx-auto bg-white border-2 border-gray-200 rounded-3xl p-5 h-auto shadow-2xl">
+        Kana
+      </div>
     </div>
   );
 }
