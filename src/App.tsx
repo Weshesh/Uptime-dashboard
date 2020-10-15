84dd4de9f08ad884e9ffd61590afff18d6d50420
Weshesh <andi.angel@boheemia.ee>
2020-10-16 01:32:48 +0300

    Add initial ProjectAvailabilityBar layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import { ProjectAvailabilityBar } from './ProjectAvailabilityBar';
 function App() {
   return (
     <div className="App bg-gray-100 flex content-center p-20 w-screen h-screen">
-      <div className="container mx-auto bg-white border-2 border-gray-200 rounded-3xl p-5 h-auto shadow-2xl">
-        Kana
-        <ProjectAvailabilityBar></ProjectAvailabilityBar>
+      <div className="container mx-auto bg-white border border-gray-200 rounded-xl h-auto shadow-2xl">
+        <ProjectAvailabilityBar projectName="Project A"></ProjectAvailabilityBar>
+        <ProjectAvailabilityBar projectName="Project B"></ProjectAvailabilityBar>
+        <ProjectAvailabilityBar projectName="Project C"></ProjectAvailabilityBar>
       </div>
     </div>
   );
--- src/ProjectAvailabilityBar.tsx
+++ src/ProjectAvailabilityBar.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
 
-export function ProjectAvailabilityBar() {
+export function ProjectAvailabilityBar(props: { projectName: string }) {
+
   return (
-    <div className="w-full border-b-2">
+    <div className="w-full border-b px-6 py-8">
+      <div className="flex justify-between">
+        <h2 className="text-gray-600 font-medium">
+          {props.projectName}
+        </h2>
+        <span className="text-green-200 font-medium">
+          Available
+        </span>
+      </div>
+      <div className="flex">
+        <div className="py-5">
+          Bar
+      </div>
+        <div className="py-5">
+          Bar
+      </div>
+        <div className="py-5">
+          Bar
+      </div>
+        <div className="py-5">
+          Bar
+      </div>
+
+      </div>
+
+      <div className="relative flex justify-center">
+        <div className="flex justify-start text-gray-300 font-medium whitespace-no-wrap">24 hours ago</div>
+        <div className="relative flex items-center mx-5 w-full" >
+            <hr className="w-full" />
+        </div>
+        <div className="flex justify-end text-gray-300 font-medium">Today</div>
+
+        <div className="absolute px-5 text-gray-500 font-medium bg-white">99.99% Availability</div>
+      </div>
 
     </div>
   )
