27cf734899f3258852772206522d5134ea5ff278
Weshesh <andi.angel@boheemia.ee>
2020-10-16 02:16:31 +0300

    Change main container height

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ function App() {
   generateDataset();
 
   return (
-    <div className="App bg-gray-100 flex content-center p-20 w-screen h-screen">
+    <div className="App bg-gray-100 flex content-center p-20 w-screen min-h-screen">
       <div className="container mx-auto bg-white border border-gray-200 rounded-xl h-auto shadow-2xl">
         <ProjectAvailabilityBar projectName="Project A"></ProjectAvailabilityBar>
         <ProjectAvailabilityBar projectName="Project B"></ProjectAvailabilityBar>
         <ProjectAvailabilityBar projectName="Project C"></ProjectAvailabilityBar>
+        <ProjectAvailabilityBar projectName="Project D"></ProjectAvailabilityBar>
+        <ProjectAvailabilityBar projectName="Project E"></ProjectAvailabilityBar>
       </div>
     </div>
   );
