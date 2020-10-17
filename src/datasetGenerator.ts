293b389755dcc6374b6525606c8d699a6c62b03e
Andi Angel <andi@bitweb.ee>
2020-10-18 01:32:56 +0300

    Add DTG date generator

--- src/datasetGenerator.ts
+++ src/datasetGenerator.ts
@@ -1,11 +1,28 @@
 interface minute {
   time: number,
-  state: 'available' | 'unavailable'
+  state?: 'available' | 'unavailable'
+}
+
+// HH:mm:ss[Z]/DDMMMYY
+
+function generateDTGDate() {
+  const date = new Date;
+  let year = date.getFullYear().toString();
+  year = year[2] + year[3]; 
+  const month = date.toLocaleString('default', { month: 'short' });
+  const day = date.getDate();
+  const dtgDate = 'Z/' + day + month + year; 
+  return dtgDate
 }
 
 function generateDataset() {
+
+
+  console.log(generateDTGDate());
+
   const startDate = new Date()
   let dataset: minute[] = []
+
   return dataset
 }
 
