20f9280164c1e00e0256380675cef06baeb1ce36
Weshesh <andi.angel@boheemia.ee>
2020-10-29 21:10:09 +0200

    Add title to the main container

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   generateDataset(sampleScenarioA);
 
   return (
-    <div className="App bg-gray-100 flex content-center p-20 w-screen min-h-screen">
+    <div className="App bg-gray-100 flex flex-col items-center content-center p-20 w-screen min-h-screen">
+        <div className='flex justify-end w-full text-gray-400 font-medium'>
+          Uptime over the past 24 hours.
+        </div>
       <div className="container mx-auto bg-white border border-gray-200 rounded-xl h-auto shadow-2xl">
         <ProjectAvailabilityBar projectName="Project A"></ProjectAvailabilityBar>
         <ProjectAvailabilityBar projectName="Project B"></ProjectAvailabilityBar>
